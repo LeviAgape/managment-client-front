3c38e89a5f017b63d0a7630b6cefc59e96b1c84d
Levi <agapelevi99@gmail.com>
2024-08-27 08:33:20 -0300

    first commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+  submitted = false;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      this.submitted = true;
+      // Aqui você pode adicionar a lógica de login, como envio ao backend
+      console.log('Login:', this.loginForm.value);
+    }
+  }
+}
